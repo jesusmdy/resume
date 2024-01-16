47bcece714fc49b908b03b3eeb7d9cba482f8746
Jesus Marcano <jesusmarcanody@gmail.com>
2024-01-16 17:30:14 -0400

    feat: include meta tags
    feat: seo optimize

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,84 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+
+const TITLE = 'Jesusmdy / Portfolio';
+const DESCRIPTION = 'Jesusmdy\'s portfolio to showcase his experience and projects involved.';
+const OG_IMAGE = {
+  url: 'https://jesusmdy.dev/images/og.png',
+  width: 1280,
+  height: 800,
+  alt: 'Jesusmdy\'s Portfolio',
+};
 
 export const metadata: Metadata = {
-  title: 'Jesusmdy / Portfolio',
-  description: 'Jesusmdy\'s portfolio to showcase his experience and projects involved.',
+  title: TITLE,
+  description: DESCRIPTION,
+  authors: {
+    url: 'https://jesusmdy.dev',
+    name: 'Jesus Marcano',
+  },
+  keywords: [
+    'jesusmdy',
+    'jesus',
+    'marcano',
+    'jesus marcano',
+    'jesusmdy dev',
+    'jesus marcano dev',
+    'jesusmdy portfolio',
+    'jesus marcano portfolio',
+    'jesusmdy dev portfolio',
+    'jesus marcano dev portfolio',
+    'jesusmdy dev portfolio',
+    'react',
+    'nextjs',
+    'typescript',
+    'javascript',
+    'portfolio',
+    'developer',
+    'software engineer',
+    'software developer',
+    'software engineering',
+    'software development',
+    'full stack',
+    'full stack developer',
+    'full stack engineer',
+    'full stack development',
+    'full stack software engineer',
+    'full stack software developer',
+    'front end',
+    'front end developer',
+    'front end engineer',
+    'front end development',
+    'front end software engineer',
+    'front end software developer',
+    'react.js',
+    'next.js',
+  ],
+  robots: 'index, follow',
+  publisher: 'Jesus Marcano',
   icons: [
     {
       rel: 'icon',
       url: '/favicon.ico',
     },
   ],
+  openGraph: {
+    siteName: TITLE,
+    title: TITLE,
+    description: DESCRIPTION,
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://jesusmdy.dev',
+    images: [OG_IMAGE],
+  },
+  twitter: {
+    creator: '@jesusmdy',
+    card: 'summary_large_image',
+    site: '@jesusmdy',
+    images: [OG_IMAGE],
+  },
 }
 
 export default function RootLayout({
